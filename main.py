9e18734958de3fcb0e1c076f9be26a7e27283e4e
Aristo <dkfud2121@naver.com>
2022-07-15 00:26:03 +0900

    feat. 데이터 셋을 이용한 api 통신

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from fastapi import FastAPI, APIRouter
+# 1 딕셔너리 형태로 예시 데이터 생성
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
+    },
+]
 
 # 1
 app = FastAPI(
@@ -17,9 +38,25 @@ def root() -> dict:
     """
     return {"msg": "Hello, World!"}
 
+# 2 - New addition, path parameter
+# https://fastapi.tiangolo.com/tutorial/path-params/
+# create endpoint /recipe/{recipe_id}
+# endpoint ? one end of a communication channel -> 채널 한쪽 끝에 해당하는 URL
+# endpoint 설정
+@api_router.get("/recipe/{recipe_id}", status_code=200)
+def fetch_recipe(*, recipe_id: int) -> dict:  # 3 defines the logic for the new endpoint
+    # endpoint 로직 정의
+    """
+    Fetch a single recipe by ID
+    """
+    # 리스트, 반복문, 조건문
+    # 4 json 형식으로 serialized 되어 반환
+    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if result:
+        return result[0]
+
 # 4
 app.include_router(api_router)
-
 
 # 5 모듈이 불릴 때 적용
 # main.py 를 실행할 때 불림
