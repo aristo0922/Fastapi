f4aa05eff93aff002f2ea9f4e0709eaea65f017d
Aristo <dkfud2121@naver.com>
2022-07-20 13:25:26 +0900

    error : 에라이 잘 되던 fastapi를 왜 인식을 못해

--- app/main.py
+++ app/main.py
@@ -1,43 +1,36 @@
-from fastapi import FastAPI, APIRouter, Query, HTTPException  # 1
+from fastapi import FastAPI, APIRouter, Query, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+
 from typing import Optional, Any
+from pathlib import Path
+
 from app.schemas import RecipeSearchResults, Recipe, RecipeCreate
+from app.recipe_data import RECIPES
 
-# 1 딕셔너리 형태로 예시 데이터 생성
-RECIPES = [
-    {
-        "id": 1,
-        "label": "Chicken Vesuvio",
-        "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
-    },
-    {
-        "id": 2,
-        "label": "Chicken Paprikash",
-        "source": "No Recipes",
-        "url": "http://norecipes.com/recipe/chicken-paprikash/",
-    },
-    {
-        "id": 3,
-        "label": "Cauliflower and Tofu Curry Recipe",
-        "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
-    },
-]
 
-app = FastAPI(
-    title="Recipe API", openapi_url="/openapi.json"
-)
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
+
+
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
 
-@api_router.get("/", status_code=200)
-def root() -> dict:
-    """
-    Root Get
-    """
-    return {"msg": "Hello, World!"}
 
-# Recipe > BaseModel > BaseModel : 상속
+# Updated to serve a Jinja2 template
+# https://www.starlette.io/templates/
+# https://jinja.palletsprojects.com/en/3.0.x/templates/#synopsis
+@api_router.get("/", status_code=200)
+def root(request: Request) -> dict:
+    """
+    Root GET
+    """
+    return TEMPLATES.TemplateResponse(
+        "index.html",
+        {"request": request, "recipes": RECIPES},
+    )
+
+
 @api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
 def fetch_recipe(*, recipe_id: int) -> Any:
     """
@@ -48,22 +41,18 @@ def fetch_recipe(*, recipe_id: int) -> Any:
     if not result:
         # the exception is raised, not returned - you will get a validation
         # error otherwise.
-        # 2
         raise HTTPException(
-            status_code=404, detail="Recipe with ID {recipe_id} not found"
+            status_code=404, detail=f"Recipe with ID {recipe_id} not found"
         )
 
     return result[0]
 
 
-
-# 1 add a response_model RecipeSerchResults to /search endpoint
 @api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
 def search_recipes(
         *,
-        # 2 Query : add additional validation and requirements to our query params (ex. min_length)
         keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
-        max_results: Optional[int] = 10
+        max_results: Optional[int] = 10,
 ) -> dict:
     """
     Search for recipes based on label keyword
@@ -73,16 +62,12 @@ def search_recipes(
         # based on the max_results query parameter
         return {"results": RECIPES[:max_results]}
 
-    # filter -> serialized to JSON by the framework
-    # lambda 매개변수:표현식 ? f(recipe)=keyword.label.lower() https://wikidocs.net/64
     results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
     return {"results": list(results)[:max_results]}
 
-# New addition, using Pydantic model `RecipeCreate` to define
-# the POST request body
-# 1
+
 @api_router.post("/recipe/", status_code=201, response_model=Recipe)
-def create_recipe(*, recipe_in: RecipeCreate) -> dict:  # 2
+def create_recipe(*, recipe_in: RecipeCreate) -> dict:
     """
     Create a new recipe (in memory only)
     """
@@ -93,14 +78,16 @@ def create_recipe(*, recipe_in: RecipeCreate) -> dict:  # 2
         source=recipe_in.source,
         url=recipe_in.url,
     )
-    RECIPES.append(recipe_entry.dict())  # 3
+    RECIPES.append(recipe_entry.dict())
 
     return recipe_entry
 
 
 app.include_router(api_router)
 
+
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
--- app/schemas.py
+++ app/schemas.py
@@ -1,16 +1,17 @@
-# 타입 힌트를 지원하기 위한 typing 모듈
-# typing ?  https://www.daleseo.com/python-typing/
-from typing import Sequence
-
 from pydantic import BaseModel, HttpUrl
 
-# Recipe : BaseModel - from pydantic- 상속
+from typing import Sequence
+
+
 class Recipe(BaseModel):
     id: int
     label: str
     source: str
-    url: HttpUrl  # 3pydantic HttpUrl 헬퍼 사용
-    #  -=> URL 컴포넌트를 어느정도 예측할 수 있도록
+    url: HttpUrl
+
+
+class RecipeSearchResults(BaseModel):
+    results: Sequence[Recipe]
 
 
 class RecipeCreate(BaseModel):
@@ -18,9 +19,3 @@ class RecipeCreate(BaseModel):
     source: str
     url: HttpUrl
     submitter_id: int
-
-# 3  uses Pydantic’s recursive capability to define a field
-#   that refers to another Pydantic class we’ve previously defined, the Recipe class
-class RecipeSearchResults(BaseModel):
-    # 4 Sequence (which is an iterable with support for len and __getitem__) of Recipes.
-    results: Sequence[Recipe]
